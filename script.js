806ad13fc58da11c4e37a9481f4c5cfdce093238
teshager <teshager8922@gmail.com>
2024-06-28 07:51:28 +0300

    add a method for setting a board cell value

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+
+const gameBoard=[['','',''],
+                 ['','',''],
+                 ['','','']];
+const setBoardCell=(position)=>{
+    gameBoard[parseInt((position-1)/3)][(position-1)%3]='X';
+    console.log(gameBoard);
+}
+
+setBoardCell(1);
