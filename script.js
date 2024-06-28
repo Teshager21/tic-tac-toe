7b8706999ed2ffcace5b4fba2cf2321a2f7ea3cd
teshager <teshager8922@gmail.com>
2024-06-28 20:15:55 +0300

    prevent the option of  overwriting previous moves

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ const players={player1:player1,player2:player2}
 
 numOfMoves=0;
 let gameOver=false;
+let move;
 currentPlayer='player2';
 
 const Board=(currentGamePlayer)=>{
@@ -34,9 +35,11 @@ const Board=(currentGamePlayer)=>{
     let board=[['','',''],['','',''],['','','']];
     return{
         setBoardCell(position){
-            board[parseInt((position-1)/3)][(position-1)%3]=players[currentPlayer].getMark();
-            currentPlayer==="player1"?currentPlayer="player2":currentPlayer="player1";
-            console.log('i was setting stuff here.',board,currentPlayer);
+            if(board[parseInt((position-1)/3)][(position-1)%3]===''){
+                board[parseInt((position-1)/3)][(position-1)%3]=players[currentPlayer].getMark();
+                currentPlayer==="player1"?currentPlayer="player2":currentPlayer="player1";
+            }
+           
         },
         resetBoard(){
             board=[['','',''],
@@ -45,6 +48,13 @@ const Board=(currentGamePlayer)=>{
         },
         getBoard(){
             return board;
+        },
+        updateBoardDisplay(){
+            for(i=0;i<3;i++){
+                for(j=0;j<3;j++){
+                    document.getElementById(`cell-${3*i+j+1}`).textContent=boardArray[i][j];
+                }
+            }
         }
 
     } 
@@ -59,26 +69,24 @@ const checkGameOver=()=>{
       //same mark diagonally
       if((boardArray[0][0]===boardArray[1][1]) && (boardArray[0][0]===boardArray[2][2])&&boardArray[0][0]!==''){
         gameOver=true;
-       alert(`game over ${currentPlayer}won`);
+       
         return;
     }
     if((boardArray[0][2]===boardArray[1][1]) && (boardArray[0][2]===boardArray[2][0])&&boardArray[0][2]!==''){
         gameOver=true;
-       alert(`game over ${currentPlayer}won`);
+       
         return;
     }
     for(i=0;i<3;i++){
         //same mark in a row
         if((boardArray[i][0]===boardArray[i][1]) && (boardArray[i][0]===boardArray[i][2])&&boardArray[i][0]!==''){
             gameOver=true;
-           alert(`game over ${currentPlayer}won`);
             break
         }
         for(j=0;j<3;j++){
             //same mark in a column
             if((boardArray[0][j]===boardArray[1][j]) && (boardArray[0][j]===boardArray[2][j])&&boardArray[0][j]!==''){
                 gameOver=true;
-               alert(`game over ${currentPlayer} won`);
                 break
             }
             //when all cells are filled
@@ -91,42 +99,22 @@ const checkGameOver=()=>{
             // gameOver
             if(counter==9){
                 gameOver=true;
-                alert(`game over ${currentPlayer}won`);
+                
             }
         }
         
     }
 }
-let move;
 
-//display
-const updateBoardDisplay=()=>{
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            let identifier=`cell-${3*i+j+1}`;
-            console.log('the id is: ',identifier)
-            document.getElementById(identifier).textContent=boardArray[i][j];
-        }
-    }
-}
 
 //even listeners
 const boardUI=document.querySelector('.board');
 boardUI.addEventListener('click',(e)=>{
-    // e.target.textContent= players[currentPlayer].getMark();
-    // console.log(e.target.getAttribute('id').slice(-1));
+    if(!gameOver){
     move=e.target.getAttribute('id').slice(-1);
-    // console.log(move);
     gameBoard.setBoardCell(parseInt(move));
-    updateBoardDisplay();
+    gameBoard.updateBoardDisplay();
     checkGameOver();
-})
-console.log(boardUI);
-// while(!gameOver){
-//     //  move=prompt('input a cell reference');
-//     console.log('you clicked the cell',move)
-//     gameBoard.setBoardCell(parseInt(move));
-//     console.log('current player before',players[currentPlayer].getMark());
-//     checkGameOver();
-//     console.log(boardArray);
-// }
+    if(gameOver) alert(`game over ${currentPlayer}won`);
+}})
+
