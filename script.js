b9e141480e7cbf494511193eb21d672dbb97a3ec
teshager <teshager8922@gmail.com>
2024-06-29 19:52:10 +0300

    keep and preserve score between rounds

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
   
-const player=(playerName,playerMark,playerScore)=>{
+const player=(playerName='',playerMark='',playerScore=0)=>{
     let mark=playerMark;
     let score=playerScore;
     let name=playerName;
@@ -20,11 +20,12 @@ const player=(playerName,playerMark,playerScore)=>{
         score=newScore;
     },
     getScore(){
-     return playerScore;
+     return score;
     }
 }
 };
 
+let numberOfRounds=0;
 const player1=player();
 const player2=player();
 player1.setMark('X');
@@ -40,10 +41,6 @@ currentPlayer='player1';
 const container=document.querySelector('.container');
 const result=container.querySelector('.result');
 
-const firstDisplayName=container.querySelector('.first-player-name');
-const secondDisplayName=container.querySelector('.second-player-name');
-const firstDisplayScore=container.querySelector('.first-player-score');
-const secondDisplayScore=container.querySelector('.second-player-score');
 
 
 
@@ -68,8 +65,14 @@ const gameBoard=(()=>{
        
     }
     updateBoardDisplay=()=>{
-        firstDisplayName.textContent=player1.getName();
-        secondDisplayName.textContent=player2.getName();
+        
+        const firstDisplayName=container.querySelector('.first-player-name');
+        const secondDisplayName=container.querySelector('.second-player-name');
+        const firstDisplayScore=container.querySelector('.first-player-score');
+        const secondDisplayScore=container.querySelector('.second-player-score');
+        console.log(player1.getScore());
+        firstDisplayName.textContent=`${player1.getName()} [${player1.getMark()}]`;
+        secondDisplayName.textContent=`${player2.getName()} [${player2.getMark()}]`
         firstDisplayScore.textContent=player1.getScore();
         secondDisplayScore.textContent=player2.getScore();
         for(i=0;i<3;i++){
@@ -88,24 +91,36 @@ const gameBoard=(()=>{
           if((board[0][0]===board[1][1]) && (board[0][0]===board[2][2])&&board[0][0]!==''){ //same mark diagonally
             gameState='over';
             winner=currentPlayer;
+            numberOfRounds++;
+            
+            const newScore=players[currentPlayer].getScore()+1;
+            console.log('before win: ',players[currentPlayer].getScore(),newScore);
+            players[currentPlayer].setScore(newScore);
+            console.log('after win: ',players[currentPlayer].getScore());
+
             return;
         }
         if((board[0][2]===board[1][1]) && (board[0][2]===board[2][0])&&board[0][2]!==''){//same mark diagonally
             gameState='over';
             winner=currentPlayer;
-           
+            numberOfRounds++;
+            players[currentPlayer].setScore(players[currentPlayer].getScore()+1);
             return;
         }
        outer_loop: for(i=0;i<3;i++){
             if((board[i][0]===board[i][1]) && (board[i][0]===board[i][2])&&board[i][0]!==''){   //same mark in a row
                 gameState='over';
                 winner=currentPlayer;
+                numberOfRounds++;
+                players[currentPlayer].setScore(players[currentPlayer].getScore()+1);
                 break
             }
             for(j=0;j<3;j++){ 
                 if((board[0][j]===board[1][j]) && (board[0][j]===board[2][j])&&board[0][j]!==''){  //same mark in a column
                     gameState='over';
                     winner=currentPlayer;
+                    numberOfRounds++;
+                    players[currentPlayer].setScore(players[currentPlayer].getScore()+1);
                     break outer_loop;
             }
                 //when all cells are filled
@@ -119,7 +134,7 @@ const gameBoard=(()=>{
                 if(counter==9){
                     gameState='over';
                     winner='tie';
-                    
+                      numberOfRounds++;  
                 }
             }
             
@@ -131,6 +146,7 @@ const gameBoard=(()=>{
             board=[['','',''],
             ['','',''],
             ['','','']];
+            updateBoardDisplay();
         },
         getBoard(){
             return board;
@@ -148,21 +164,21 @@ gameBoard.bindEvent();
 
 //dialog
 const playBtn=document.querySelector('.play');
-playBtn.addEventListener('click',()=>{
-        
+playBtn.addEventListener('click',()=>{   
         nameInputDialog.showModal();
+        gameBoard.resetBoard();
         gameState='on';
     
 })
-const nameInputDialog= document.querySelector('#nameInputDialog');
+const nameInputDialog= container.querySelector('#nameInputDialog');
 const closeDialogBtn= nameInputDialog.querySelector('#closeDialog')
 const firstPlayerName=nameInputDialog.querySelector('#playerName1')
 const secondPlayerName=nameInputDialog.querySelector('#playerName2')
 const confirmNameBtn=nameInputDialog.querySelector('#confirmNameBtn')
 
 closeDialogBtn.addEventListener('click',()=>{nameInputDialog.close();}) //close event
+const captureDialogReturnValue=(e)=>{
 
-confirmNameBtn.addEventListener('click',(e)=>{
     e.preventDefault();
     if(firstPlayerName.value!==''&&secondPlayerName.value!==''){
         nameInputDialog.close([firstPlayerName.value,secondPlayerName.value]);
@@ -172,7 +188,11 @@ confirmNameBtn.addEventListener('click',(e)=>{
         gameBoard.updateBoardDisplay();
     }
    
-})
+}
+
+confirmNameBtn.addEventListener('click',captureDialogReturnValue)
+
+
 
 
 
