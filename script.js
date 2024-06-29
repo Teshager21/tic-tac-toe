c2abf7f1ab2f2080343b86692f88698d549a9730
teshager <teshager8922@gmail.com>
2024-06-29 15:00:37 +0300

    refactor code to make gameBoard an imediatetly invoked factory function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-const playerFactory=(playerName,playerMark,playerScore)=>{
+  
+const player=(playerName,playerMark,playerScore)=>{
     let mark=playerMark;
     let score=playerScore;
     let name=playerName;
@@ -25,29 +25,91 @@ const playerFactory=(playerName,playerMark,playerScore)=>{
 }
 };
 
-const player1=playerFactory('',0);
-const player2=playerFactory();
+const player1=player();
+const player2=player();
 player1.setMark('X');
 player2.setMark('O');
 const players={player1:player1,player2:player2}
-// console.log('ma player is ',players['player1'].getMark());
 
 numOfMoves=0;
-let gameOver=false;
+let gameState='';
 let move;
-currentPlayer='player2';
+currentPlayer='player1';
 
-const Board=(currentGamePlayer)=>{
-    let currentPlayer=currentGamePlayer;
+const container=document.querySelector('.container');
+const result=container.querySelector('.result');
+
+
+const gameBoard=(()=>{
+    const boardUI=container.querySelector('.board');
     let board=[['','',''],['','',''],['','','']];
-    return{
-        setBoardCell(position){
-            if(board[parseInt((position-1)/3)][(position-1)%3]===''){
-                board[parseInt((position-1)/3)][(position-1)%3]=players[currentPlayer].getMark();
-                currentPlayer==="player1"?currentPlayer="player2":currentPlayer="player1";
+    const clickHandler=(e)=>{
+        if(gameState==='on'){
+            move=parseInt(e.target.getAttribute('id').slice(-1));
+            setBoardCell(move);
+            updateBoardDisplay();
+            runGameLogic();
+            updateBoardDisplay();    
+    }}
+    setBoardCell=(position)=>{
+        if(board[parseInt((position-1)/3)][(position-1)%3]===''){
+            board[parseInt((position-1)/3)][(position-1)%3]=players[currentPlayer].getMark();
+            currentPlayer==="player1"?currentPlayer="player2":currentPlayer="player1";
+        }
+       
+    }
+    updateBoardDisplay=()=>{
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                boardUI.querySelector(`#cell-${3*i+j+1}`).textContent=board[i][j];
             }
+        }
+        if(gameState==='over') result.textContent=`Game over! ${players[currentPlayer].getName()} won`;
+    }
+    runGameLogic=()=>{
+       console.log('i was called');
+        counter=0;
+          //same mark diagonally
+          if((board[0][0]===board[1][1]) && (board[0][0]===board[2][2])&&board[0][0]!==''){
+            gameState='over';
            
-        },
+            return;
+        }
+        if((board[0][2]===board[1][1]) && (board[0][2]===board[2][0])&&board[0][2]!==''){
+            gameState='over';
+           
+            return;
+        }
+        for(i=0;i<3;i++){
+            //same mark in a row
+            if((board[i][0]===board[i][1]) && (board[i][0]===board[i][2])&&board[i][0]!==''){
+                gameState='over';
+                break
+            }
+            for(j=0;j<3;j++){
+                //same mark in a column
+                if((board[0][j]===board[1][j]) && (board[0][j]===board[2][j])&&board[0][j]!==''){
+                    gameState='over';
+                    break
+                }
+                //when all cells are filled
+                if(board[i][j]===''){
+                    gameState='on';
+                    break;
+                }else{
+                    counter++;
+                }
+                // gameOver
+                if(counter==9){
+                    gameState='over';
+                    
+                }
+            }
+            
+        }
+    }
+    return{setBoardCell,updateBoardDisplay,runGameLogic
+        ,
         resetBoard(){
             board=[['','',''],
             ['','',''],
@@ -56,91 +118,33 @@ const Board=(currentGamePlayer)=>{
         getBoard(){
             return board;
         },
-        updateBoardDisplay(){
-            for(i=0;i<3;i++){
-                for(j=0;j<3;j++){
-                    document.getElementById(`cell-${3*i+j+1}`).textContent=boardArray[i][j];
-                }
-            }
+         
+        bindEvent(){
+            boardUI.addEventListener('click',clickHandler,false);
         }
+
 
     } 
-};
-// console.log('curren player when called',players[currentPlayer].getMark());
-const gameBoard= Board(currentPlayer);
-boardArray=gameBoard.getBoard();
-//gameOver conditions
-const runGameLogic=()=>{
-    
-    counter=0;
-      //same mark diagonally
-      if((boardArray[0][0]===boardArray[1][1]) && (boardArray[0][0]===boardArray[2][2])&&boardArray[0][0]!==''){
-        gameOver=true;
-       
-        return;
-    }
-    if((boardArray[0][2]===boardArray[1][1]) && (boardArray[0][2]===boardArray[2][0])&&boardArray[0][2]!==''){
-        gameOver=true;
-       
-        return;
-    }
-    for(i=0;i<3;i++){
-        //same mark in a row
-        if((boardArray[i][0]===boardArray[i][1]) && (boardArray[i][0]===boardArray[i][2])&&boardArray[i][0]!==''){
-            gameOver=true;
-            break
-        }
-        for(j=0;j<3;j++){
-            //same mark in a column
-            if((boardArray[0][j]===boardArray[1][j]) && (boardArray[0][j]===boardArray[2][j])&&boardArray[0][j]!==''){
-                gameOver=true;
-                break
-            }
-            //when all cells are filled
-            if(boardArray[i][j]===''){
-                gameOver=false;
-                break;
-            }else{
-                counter++;
-            }
-            // gameOver
-            if(counter==9){
-                gameOver=true;
-                
-            }
-        }
-        
-    }
-}
+})();
 
-
-//even listeners
-const result=document.querySelector('.result');
-const boardUI=document.querySelector('.board');
-boardUI.addEventListener('click',(e)=>{
-    if(!gameOver){
-    move=e.target.getAttribute('id').slice(-1);
-    gameBoard.setBoardCell(parseInt(move));
-    gameBoard.updateBoardDisplay();
-    runGameLogic();
-    if(gameOver) result.textContent=`Game over! ${players[currentPlayer].getName()} won`;
-    gameBoard.updateBoardDisplay();
-
-}})
+gameBoard.bindEvent();
 
 //dialog
 const playBtn=document.querySelector('.play');
 playBtn.addEventListener('click',()=>{
         
         nameInputDialog.showModal();
+        gameState='on';
     
 })
 const nameInputDialog= document.querySelector('#nameInputDialog');
 const closeDialogBtn= nameInputDialog.querySelector('#closeDialog')
 const firstPlayerName=nameInputDialog.querySelector('#playerName1')
 const secondPlayerName=nameInputDialog.querySelector('#playerName2')
-closeDialogBtn.addEventListener('click',()=>{nameInputDialog.close();}) //close event
 const confirmNameBtn=nameInputDialog.querySelector('#confirmNameBtn')
+
+closeDialogBtn.addEventListener('click',()=>{nameInputDialog.close();}) //close event
+
 confirmNameBtn.addEventListener('click',(e)=>{
     e.preventDefault();
     nameInputDialog.close([firstPlayerName.value,secondPlayerName.value]);
